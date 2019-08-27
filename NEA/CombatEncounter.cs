072e7589a1d4631c0bdc19c8dc381b9f8ceff18c
meepedmeepo <43563948+meepedmeepo@users.noreply.github.com>
2019-08-27 21:57:29 +0100

    Added Encounter and CombatEncounter classes to the project

---
+++ NEA/CombatEncounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace NEA
+{
+    [Serializable]
+    public class CombatEncounter : Encounter,ISerializable
+    {
+        public List<Character> Enemies { get; set; }
+
+        public override void RunEncounter()//TODO: Complete
+        {
+            Console.WriteLine("Combat encounter");
+        }
+        public CombatEncounter(int Reward,List<Character> Enemies) : base(Reward)
+        {
+            this.Enemies = Enemies;
+        }
+        public CombatEncounter(int Reward) : base(Reward)
+        {
+            this.Enemies = new List<Character>();
+        }
+        public CombatEncounter(SerializationInfo info, StreamingContext context)
+        {
+            this.Reward = (int)info.GetValue("Reward", typeof(int));
+            this.Enemies = (List<Character>)info.GetValue("Enemies", typeof(List<Character>));
+        }
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Enemies", this.Enemies);
+            base.GetObjectData( info, context);
+
+
+        }
+    }
+}
---
+++ NEA/Encounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace NEA
+{
+    [Serializable]
+    public abstract class Encounter : ISerializable
+    {
+        public int Reward { get; set; }
+        public Encounter(int Reward)
+        {
+            this.Reward = Reward;
+        }
+
+        public virtual void RunEncounter()
+        {
+            Console.WriteLine("Test Encounter");
+        }
+        public Encounter(SerializationInfo info, StreamingContext context)
+        {
+            this.Reward = (int)info.GetValue("Reward", typeof(int));
+            
+        }
+        public Encounter()
+        { }
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Reward", this.Reward);
+         
+        }
+    }
+}
